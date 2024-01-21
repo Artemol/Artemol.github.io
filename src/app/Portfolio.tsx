d0ba0b2ae48b3e556bfc84a7dbe9d9a93eea9412
Artemol <67518760+Artemol@users.noreply.github.com>
2024-01-22 02:25:31 +0900

    feat: 🎸 Added Radix UI
    Added Radix UI by following Getting-Started.

---
+++ src/app/Portfolio.tsx
@@ -0,0 +1,12 @@
+import { Button, Flex, Text } from "@radix-ui/themes";
+
+const Portfolio = () => {
+  return (
+    <Flex direction="column" gap="2">
+      <Text>Hello from Radix Themes {":)"}</Text>
+      <Button>Let&apos;s go</Button>
+    </Flex>
+  );
+};
+
+export default Portfolio;
